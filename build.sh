355aad68389f5e5eb10664f5db986ba9a77df5c1
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2026-03-27 17:18:35 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,9 +15,10 @@ rm -rf vendor/infinity-priv/keys
 rm -rf vendor/qcom/opensource/core-utils
 rm -rf device/qcom/common
 rm -rf vendor/qcom/common
-rm -rf device/qcom/qssi
 rm -rf packages/apps/KProfiles
 rm -rf hardware/qcom/display
+rm -rf hardware/qcom/media
+rm -rf hardware/qcom/audio
 rm -rf device/qcom/sepolicy_vndr
 
 # Clone sources
@@ -30,9 +31,13 @@ git clone https://github.com/ShahzebQureshi/ak vendor/lineage-priv/keys --depth=
 git clone https://github.com/yaap/vendor_qcom_opensource_core-utils -b sixteen vendor/qcom/opensource/core-utils --depth=1
 git clone https://github.com/yaap/device_qcom_common -b sixteen device/qcom/common --depth=1
 git clone https://gitlab.com/yaosp/vendor_qcom_common -b sixteen vendor/qcom/common --depth=1
-git clone https://github.com/AOSPA/android_device_qcom_qssi -b vauxite device/qcom/qssi --depth=1
 git clone https://github.com/yaap/packages_apps_KProfiles -b sixteen packages/apps/KProfiles --depth=1
 git clone https://github.com/yaap/hardware_qcom-caf_sm8150_display -b sixteen hardware/qcom/display --depth=1
+git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media -b sixteen hardware/qcom/media --depth=1
+git clone https://github.com/yaap/hardware_qcom-caf_sm8150_audio -b sixteen hardware/qcom/audio --depth=1
+git clone https://github.com/yaap/device_qcom_common -b sixteen device/qcom/common --depth=1
+git clone https://gitlab.com/yaosp/vendor_qcom_common -b sixteen vendor/qcom/common --depth=1
+
 git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr -b lineage-23.2-legacy-um device/qcom/sepolicy_vndr --depth=1
 # Clone hardware LAST
 git clone https://github.com/LineageOS/android_hardware_oneplus hardware/oneplus --depth=1
