924ec4b65c32d485053d1d4ab00c31995b83d639
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2026-06-12 11:05:43 +0500

    Switch to quinces trees

--- build.sh
+++ build.sh
@@ -14,13 +14,13 @@ rm -rf hardware/oplus
 
 # Clone sources
 git clone https://github.com/ShahzebQureshi/device_oneplus_guacamole-Evox -b Infinity device/oneplus/guacamole --depth=1
-git clone https://github.com/Codecity001/device_oneplus_sm8150-common-1 device/oneplus/sm8150-common --depth=1
-git clone https://github.com/Codecity001/android_kernel_oneplus_sm8150 kernel/oneplus/sm8150 --depth=1
+git clone https://github.com/QuinceROMs/device_oneplus_sm8150-common-1 -b 16-d-cam device/oneplus/sm8150-common --depth=1
+git clone https://github.com/QuinceROMs/android_kernel_oneplus_sm8150 -b 16-c kernel/oneplus/sm8150 --depth=1
 git clone https://github.com/ShahzebQureshi/proprietary_vendor_oneplus_guacamole -b Infinity vendor/oneplus/guacamole --depth=1
-git clone https://github.com/Codecity001/vendor_oneplus_sm8150-common-1 vendor/oneplus/sm8150-common --depth=1
+git clone https://github.com/QuinceROMs/vendor_oneplus_sm8150-common-1 -b 16-d-cam vendor/oneplus/sm8150-common --depth=1
 
 # Clone hardware LAST
-git clone https://github.com/Codecity001/hardware_oneplus-1 hardware/oplus --depth=1
+git clone https://github.com/QuinceROMs/hardware_oneplus-1 -b 16-cam hardware/oplus --depth=1
 
 # Set up build environment
 export BUILD_USERNAME=ShahzebQureshi
